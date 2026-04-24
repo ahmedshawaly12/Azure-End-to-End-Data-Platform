ff9b35038ac8f288f541d78f47cfffb42e14234d
ahmedshawaly12 <ahmed.shawaly151@gmail.com>
2026-04-25 02:29:10 +0300

    create fast api

---
+++ fast_api/api.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from data import generate_uber_ride_confirmation
+
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def booking_home(request: Request):
+    return templates.TemplateResponse(request=request, name="home.html")
+
+@app.get("/book")
+def book_ride(request: Request):
+    
+    uber_data = generate_uber_ride_confirmation()
+    print("data generated sucessfully")
+
+    return templates.TemplateResponse(request=request, name="confirmation.html")
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
